17999af73f4562a7c49f6a57c566a33d32eacfcb
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2024-08-04 19:32:49 +0700

    feat: 🎸 new for get ticket details and when not found to
    with generarticStaticParams

---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,47 @@
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true; // default val = true
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
+async function getTicket(id) {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 0, // use 0 to opt out of using cache
+    },
+  });
+
+  if (!res.ok) {
+    notFound();
+  }
+
+  return res.json();
+}
+
+export default async function TicketDetails({ params }) {
+  // cosnt id
+  const ticket = await getTicket(params.id);
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created By {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
+}
